f5e41bcb09e033370a666727acbe6c7e5c0edc36
Lopez Jose Angel MNR UDMA <joseangel.lopez@zf.com>
2023-05-23 21:44:07 -0600

    A new pointer example was added to the file

--- punterosTest1.py
+++ punterosTest1.py
@@ -6,3 +6,8 @@ print(b is a) # resultado: True
 a = 6
 print("a = {}; b = {}".format(a,b)) # (2)
 print(b is a) # resultado: False
+
+numero = 1
+otroNumero = 2
+print(id(numero))
+print(id(otroNumero))
