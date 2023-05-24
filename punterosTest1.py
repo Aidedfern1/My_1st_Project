8e625985a58147eef8d16363699c0be320f12009
Lopez Jose Angel MNR UDMA <joseangel.lopez@zf.com>
2023-05-24 11:32:31 -0600

    A second function example was added

--- punterosTest1.py
+++ punterosTest1.py
@@ -1,3 +1,5 @@
+import math as m
+
 a = 5
 b = a
 print("a = {}; b = {}".format(a,b)) # (1)
@@ -15,5 +17,12 @@ print(id(otroNumero))
 def fun():
   print("Hello There!")
 
+def CircleArea(r:float) -> float:
+  area=m.pi*(r**2)
+
+  return area
 
 fun()
+r= 4
+ans=CircleArea(r)
+print("The area of your circle is {}".format(ans))
